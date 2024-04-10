8e9d30b2b8cc4f497e615bc2d382795a9704efd3
ryu <ryudonghyun4918@naver.com>
2024-04-10 23:20:19 +0900

    test commit

---
+++ profile.js
@@ -0,0 +1,12 @@
+// script.js
+document.getElementById("introductionBtn").addEventListener("click", function() {
+  window.location.href = "INTRODUCE.html";
+});
+
+document.getElementById("infoBtn").addEventListener("click", function() {
+  window.location.href = "Ryu's TMI.html";
+});
+
+document.getElementById("plansBtn").addEventListener("click", function() {
+  window.location.href = "Plan.html";
+});
